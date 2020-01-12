fa5ab93b753ac5540ff068b3fa4a044425c80504
JArmour416 <jennyarmour98@gmail.com>
2020-01-11 19:07:48 -0500

    1st attempt

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,36 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime");
+
+ // Get a reference to the table body
+var tbody = d3.select("tbody");
+
+function autoPopulate(tableData) {
+    tableData.forEach((visits) => {
+        var row = tbody.append("tr");
+        Object.entries(visits).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    };
+}
+
+button.on("click", function() {
+    d3.evet.preventDefault();
+    tbody.html("");
+     var inputElement = d3.select("#filtertype");
+     var inputValue = inputElement.property("value");
+})
+
+filteredData.forEach((alients) => {
+    var row = tbody.append("tr");
+    Object.entries(visits).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+}
